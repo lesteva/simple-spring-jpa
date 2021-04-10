63213d8edcf6cb720d690382df0908d44806b4c6
esteva9010 <esteva9010@gmail.com>
2021-04-10 14:32:05 -0500

    se modifica data.sql y agrega profile para desa

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,4 @@
 insert into Client (id,firstname,lastname,age)values(1, 'Alex','lopez',29);
 insert into Client (id,firstname,lastname,age)values(2, 'Julio','G',31);
 insert into Client (id,firstname,lastname,age)values(3, 'César','Martinez',28);
+insert into Client (id,firstname,lastname,age)values(4, 'JC','Lpz',27);
