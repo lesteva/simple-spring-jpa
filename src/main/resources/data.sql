2e76331c60d14a894ca71ea04e6f7fffdae40909
esteva9010 <esteva9010@gmail.com>
2021-04-10 18:20:55 -0500

    conflic master

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,4 @@ insert into Client (id,firstname,lastname,age)values(1, 'Alex','lopez',29);
 insert into Client (id,firstname,lastname,age)values(2, 'Julio','G',31);
 insert into Client (id,firstname,lastname,age)values(3, 'César','Martinez',28);
 insert into Client (id,firstname,lastname,age)values(4, 'JC','LE',29);
+insert into Client (id,firstname,lastname,age)values(6, 'JC','Master',29);
